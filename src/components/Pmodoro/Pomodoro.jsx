b4200690bc69ab1a3d471fa1e7469d8c651ac81f
Krzysztof Pich <kontakt@pich.pl>
2024-06-22 13:37:02 +0200

    add basic controls as wireframe

--- src/components/Pmodoro/Pomodoro.jsx
+++ src/components/Pmodoro/Pomodoro.jsx
@@ -1,9 +1,17 @@
 const Pomodoro = () => {
     return (
         <>
-            <button>start</button>
-            <button>pause</button>
-            <button>stop</button>
+            <div id="timer-type">
+                <button>Work</button>
+                <button>Short break</button>
+                <button>Long break</button>
+            </div>
+            <p>25:00</p>
+            <div id="timer-operations">
+                <button>start</button>
+                <button>pause</button>
+                <button>stop</button>
+            </div>
         </>
     )
 }
