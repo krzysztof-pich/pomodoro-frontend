e19b365e44c72fe53c54030dda0e0ae7631563b5
Krzysztof Pich <kontakt@pich.pl>
2024-06-26 23:01:18 +0200

    Header styling

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -1,8 +1,18 @@
+import {AppBar, Box, IconButton, Toolbar, Typography} from "@mui/material";
+import AvTimerIcon from '@mui/icons-material/AvTimer';
+
 const Header = () => {
     return (
-        <header>
-            <p>Pomodoro application</p>
-        </header>
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton><AvTimerIcon/></IconButton>
+                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                        Pomodoro
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
 
