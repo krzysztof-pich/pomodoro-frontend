c1952dc88ba2c07ef9504c2165211484a5e4337d
Krzysztof Pich <krzysztofpich@gorillagroup.com>
2024-08-15 12:45:44 +0200

    stages statistics with basic tests

--- src/components/Pmodoro/StageStatistics.jsx
+++ src/components/Pmodoro/StageStatistics.jsx
@@ -2,6 +2,20 @@
  * @typedef {import('../../typedefs').ActionLog} ActionLog
  */
 
+import {getPomodorosFromActions} from "../../services/actions";
+
+/**
+ *
+ * @param {string} state
+ * @returns {string}
+ */
+const getContentFromPomodoroState = (state) => {
+    if (state === 'completed') {
+        return '(P)';
+    }
+    return '(...P...)';
+}
+
 /**
  *
  * @param {ActionLog[]} actions
@@ -9,7 +23,13 @@
  * @constructor
  */
 const StageStatistics = ({actions}) => {
-    return <p>(P)(P)(P)(P)(LB)</p>
+    const pomodoros = getPomodorosFromActions(actions);
+    let i = 0;
+    return (<p data-testid="pomodoro-statistics-box">
+        {pomodoros.filter((p) => p.stage === 'work').map((p) => {
+            return (<span key={i += 1}>{getContentFromPomodoroState(p.state)}</span>);
+        })}
+    </p>);
 }
 
 export default StageStatistics
